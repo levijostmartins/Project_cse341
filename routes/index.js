6ae93ae8d764909bff18102d9e0e9720c7360b6a
levi <jostlevi@hotmail.com>
2025-11-07 14:08:57 -0300

    finshed week01

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const usersRouter = require('./users'); 
 
+router.get('/', (req, res) => {res.send('hello world');});
 router.use('/users', usersRouter);
 
 module.exports = router;
