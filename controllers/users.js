1014f714ebfcc915d9fb1354ce4fd8ca6ded2da2
levi <jostlevi@hotmail.com>
2025-11-15 09:42:25 -0300

    render

--- controllers/users.js
+++ controllers/users.js
@@ -19,55 +19,54 @@ const getSingle = async (req, res) => {
 };
 
 const createUser = async (req, res) => {
-    const userId = new ObjectId(req.params.id.trim());
-    const user = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday,
-    };
-    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
-    if (response.acknowledged) {
-        res.status(204).send();
-    }
-    else {
-        res.status(500).json(response.error || 'some error has occured while updating the user')
-    }
-}
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
 
+  const response = await mongodb.getDatabase().collection('users').insertOne(user);
 
-    
+  if (response.acknowledged) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'some error has occured while updating the user');
+  }
+};
+
 const updateUser = async (req, res) => {
-    const userId = new ObjectId(req.params.id.trim());
-       const user = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday,
-    };
-        const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id: userId }, user);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    }
-    else {
-        res.status(500).json(response.error || 'some error has occured while updating the user')
-    }
-}
+  const userId = new ObjectId(req.params.id);
 
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  };
+
+  const response = await mongodb.getDatabase().collection('users').replaceOne({ _id: userId }, user);
+
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'some error has occured while updating the user');
+  }
+};
 
 const deleteUser = async (req, res) => {
-    const userId = new ObjectId(req.params.id.trim())
-    const response = await mongodb.getDatabase().db().collection('users').remove({ _id: userId }, true);
-    if (response.deleteCount > 0) {
-        res.status(204).send();
-    }
-    else {
-        res.status(500).json(response.error || 'some error has occured while updating the user')
-    }
+  const userId = new ObjectId(req.params.id);
 
-}
+  const response = await mongodb.getDatabase().collection('users').deleteOne({ _id: userId });
+
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'some error has occured while updating the user');
+  }
+};
 
 module.exports = {
   getAll,
